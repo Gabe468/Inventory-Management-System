3460279385aa6c37c5bc543e8c3ca937642faebb
Gabe <gabemendoza27@gmail.com>
2023-10-21 21:03:19 -0400

    Added React

--- backend/frontend/src/pages/InventoryListPage.js
+++ backend/frontend/src/pages/InventoryListPage.js
@@ -1,10 +1,27 @@
 import React, {useState, useEffect} from 'react'
+import ListItem from '../components/ListItem'
 
 const InventoryListPage = () => {
-    let [inventory, setInventory] = useState([])
+    let [items, setInventory] = useState([])
+
+    useEffect(()=>{
+      getInventory()
+    }, [])
+ 
+    let getInventory = async () => {
+      let response = await fetch('http://127.0.0.1:8000/api/item/')
+      let data = await response.json()
+      setInventory(data)
+
+    }
+
   return (
     <div>
-        inventory
+        <div className='inventory-list'>
+              {items.map((item, index) => (
+                <ListItem key={index} item={item} />
+            ))}
+        </div>
     </div>
   )
 }
