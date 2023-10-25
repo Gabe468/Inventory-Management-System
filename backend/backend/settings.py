9924a32d59bdc94adaf169610ca9380a3e388fb9
Gabe <gabemendoza27@gmail.com>
2023-10-24 21:40:53 -0400

    linking django and react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                BASE_DIR / 'frontend/build'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from inventoryManage import views
 from rest_framework import routers
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'item', views.InventoryView, 'item')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls))
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
