ce13610eb32cf92f641a1138e3dab4d6b53c31c6
Gabe <gabemendoza27@gmail.com>
2023-10-27 08:17:45 -0400

    Adding react-grid

--- backend/frontend/src/components/ListItem.js
+++ backend/frontend/src/components/ListItem.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import GridLayout from "react-grid-layout"
 
 const ListItem = ({ item }) => {
   return (
     <Link to={`/item/${item.id}`}>
-      <div className='item-list-item'>
-        <h3>{item.title}</h3>
-      </div>
+      <GridLayout className="layout" cols={5} rowHeight={35} width={'1000'}>
+      <div className='item-list-item' key="list" data-grid={{ x: 0, y: 0, w: 1, h: 1, static: true }}>
+      <h3>{item?.id}</h3>
+        </div>
+        <div className='item-list-item' key="title" data-grid={{ x: 1, y: 0, w: 1, h: 1, static: true }}>
+      <h3>{item?.title}</h3>
+        </div>
+        <div className='item-list-item' key="description" data-grid={{ x: 2, y: 0, w: 1, h: 1, static: true }}>
+      <h3>{item?.description}</h3>
+        </div>
+        <div className='item-list-item' key="sku" data-grid={{ x: 3, y: 0, w: 1, h: 1, static: true }}>
+      <h3>{item?.sku}</h3>
+        </div>
+        <div className='item-list-item' key="count" data-grid={{ x: 4, y: 0, w: 1, h: 1, static: true }}>
+      <h3>{item?.count}</h3>
+        </div>
+      </GridLayout>
     </Link>
   )
 }
