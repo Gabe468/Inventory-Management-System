55dd90c5c3b0cb1ff4c4a642b81faab98e1d8fd8
Gabriel Mendoza <gabemendoza27@gmail.com>
2023-11-03 15:28:55 -0400

    Added changes to list page

--- backend/frontend/src/pages/InventoryPage.js
+++ backend/frontend/src/pages/InventoryPage.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
 
 const InventoryPage = ({ match, history }) => {
 
@@ -14,6 +15,10 @@ const InventoryPage = ({ match, history }) => {
         let data = await response.json()
         setItem(data)
     }
+    const navigate = useNavigate();
+    const goBack = () => {
+      navigate(-1);
+    };
 
     let deleteItem = async ()=>{
       fetch('/api/item/${itemId}/delete/',{
@@ -28,9 +33,20 @@ const InventoryPage = ({ match, history }) => {
   return (
     <div className='item'>
       <div className='item-header'>
+        <button onClick={goBack}>back</button>
         <button onClick={deleteItem}>Delete</button>
       </div>
-      <h3>{item?.id}{item?.title}{item?.description}{item?.sku}{item?.count}</h3>
+      <div className='item-group'>
+      <h3 className='item'>ID: {item?.id}</h3>
+      <h3 className='item'>Title:</h3>
+      <h3 className='item'>{item?.title}</h3>
+      <h3 className='item'>Description:</h3>
+      <h3 className='item'>{item?.Description}</h3>
+      <h3 className='item'>SKU:</h3>
+      <h3 className='item'>{item?.sku}</h3>
+      <h3 className='item'>Count:</h3>
+      <h3 className='item'>{item?.count}</h3>
+    </div>
     </div>
   )
 }
